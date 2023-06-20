d53321c3fc5063549e4be285d1e21f7c2a0f621e
AndreyUA <infernalsteel666@gmail.com>
2023-06-20 23:06:36 +0300

    reconfigured app

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
 import { Application } from "pixi.js";
 
 // Create the application
-const app = new Application();
-
-// Add the view to the DOM
-document.body.appendChild(app.view);
+const app = new Application({ view: document.getElementById("canvas") });
